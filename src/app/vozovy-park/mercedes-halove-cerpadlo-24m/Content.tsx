e4baf5b92d1fa43b91f291e7be4bb4db29651826
Keny2802 <oliva.vojta@gmail.com>
2026-02-22 11:19:47 +0100

    Přidání technické listy do Mercedes Halové čerpadlo 24 m.

--- src/app/vozovy-park/mercedes-halove-cerpadlo-24m/Content.tsx
+++ src/app/vozovy-park/mercedes-halove-cerpadlo-24m/Content.tsx
@@ -1,4 +1,7 @@
+"use client";
+
 import {
+    useState,
     Fragment
 } from "react";
 
@@ -9,14 +12,24 @@ import FlexCol from "@/app/components/FlexCol";
 import Heading from "@/app/components/Heading";
 import BodyText from "@/app/components/BodyText";
 import MarginTop from "@/app/components/MarginTop";
+import Flex from "@/app/components/Flex";
 import Grid from "@/app/components/Grid";
 import List from "@/app/components/List";
+import Img from "@/app/components/Img";
 import ListItem from "@/app/components/ListItem";
 import About from "@/app/sections/About";
 import Contact from "@/app/sections/Contact";
 import Footer from "@/app/sections/Footer";
+import SwipeSlider from "@/app/components/SwipeSlider";
+
+const images = [
+    "/fota/sekce/vozovy-park/vozovy-park-1-technicke-listy.jpg"
+]
 
 const Content = () => {
+    const [imgIndex, setImgIndex] = useState<number | null>(0);
+    const [isClosed, setToClosed] = useState<boolean>(false);
+
     return (
         <Fragment>
             <ContactHeader />
@@ -31,33 +44,56 @@ const Content = () => {
                     </BodyText>
                 </FlexCol>
                 <MarginTop>
-                    <List>
-                        <Grid className="grid-cols-1 md:grid-cols-2 gap-2.5 md:gap-3 lg:gap-4">
-                            {[
-                                `Podvozek: Mercedes-Benz nákladní podvozek 3 - 4 nápravový.`,
-                                `Dosah ramene: 24 m (4 dílné hydraulické výložní rameno).`,
-                                `Výkon čerpadla: cca 90 - 120 m³/h (dle konfigurace čerpací jednotky).`,
-                                `Čerpací tlak: až 80 bar.`,
-                                `Typ čerpadla: dvoupístové hydraulické betonové čerpadlo.`,
-                                `Průměr potrubí: standardně DN 125 mm.`,
-                                `Výška čerpání: cca 20 - 24 m.`,
-                                `Délka vodorovného čerpání: cca 150 - 250 m.`,
-                                `Stabilizace: hydraulické výsuvné opěry.`,
-                                `Emisní norma motoru: Euro 5 / Euro 6 (dle roku výroby).`
-                            ].map((textBlock, index) => {
-                                return (
-                                    <ListItem key={index}>
-                                        {textBlock}
-                                    </ListItem>
-                                );
-                            })}
-                        </Grid>
-                    </List>
+                    <Flex className="justify-center flex-col-reverse gap-2 md:gap-4 lg:gap-6">
+                        <List>
+                            <Grid className="grid-cols-1 md:grid-cols-2 gap-2.5 md:gap-3 lg:gap-4">
+                                {[
+                                    `Podvozek: Mercedes-Benz nákladní podvozek 3 nápravový.`,
+                                    `Dosah ramene: 24 m (4 dílné hydraulické výložní rameno).`,
+                                    `Výkon čerpadla: 140 m³ / h.`,
+                                    `Čerpací tlak: až 80 bar.`,
+                                    `Typ čerpadla: dvoupístové hydraulické betonové čerpadlo.`,
+                                    `Průměr potrubí: standardně DN 125 mm.`,
+                                    `Výška čerpání: 24 m.`,
+                                    `Délka vodorovného čerpání: 20 m.`,
+                                    `Stabilizace: hydraulické výsuvné opěry.`,
+                                    `Emisní norma motoru: Euro 6.`
+                                ].map((textBlock, index) => {
+                                    return (
+                                        <ListItem key={index}>
+                                            {textBlock}
+                                        </ListItem>
+                                    );
+                                })}
+                            </Grid>
+                        </List>
+                        <Img
+                        width={400}
+                        height={400}
+                        onClick={() => {
+                            setImgIndex(3);
+                            setToClosed(false);
+                        }}
+                        src="/fota/sekce/vozovy-park/vozovy-park-1-technicke-listy.jpg"
+                        alt="Mercedes Halové čerpadlo 24 m Technické listy společnosti AD Podlahy"
+                        className="w-full md:max-w-[400px] object-cover rounded-md"
+                        />
+                    </Flex>
                 </MarginTop>
             </Section>
             <About />
             <Contact />
             <Footer />
+            {
+                imgIndex !== null && !isClosed && (
+                    <SwipeSlider
+                    src={images}
+                    startIndex={imgIndex ?? 0}
+                    isClosed={isClosed}
+                    setToClosed={setToClosed}
+                    />
+                )
+            }
         </Fragment>
     );
 };
